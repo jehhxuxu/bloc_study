0008d2879ca3f339ef5710e276c8f97d46525d78
jehpereira2017 <je.mary0804@gmail.com>
2021-01-18 20:49:03 -0300

    desafio aula 27

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:desafio27/app/app_module.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Color(0xFFFF5555),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Image.asset(
+                    'assets/images/tinder.png',
+                    fit: BoxFit.contain,
+                  ),
+                  height: 200,
+                  width: 200,
+                ),
+                Text(
+                  'Location Changer',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Plugin app for Tinder',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    width: 220,
+                    height: 50,
+                    color: Colors.white,
+                    child: Center(
+                      child: Text(
+                        'Login with Facebook',
+                        style: TextStyle(
+                          color: Color(0xFFFF5555),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
